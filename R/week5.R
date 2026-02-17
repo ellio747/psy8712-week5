7703e4a8b557fc61b200a71bb1748a99dd020c32
Ben Elliott <ellio747@umn.edu>
2026-02-17 16:11:54 -0600

    need to fix L8 or L9

--- R/week5.R
+++ R/week5.R
@@ -6,7 +6,7 @@ library(tidyverse)
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q",1:10)))
-Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = TRUE, col_types = "ic")
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = TRUE, col_types = "ic") # One of these needs to be read_table()
 
 # Data Cleaning
 Aclean_tbl <- Adata_tbl %>% 
