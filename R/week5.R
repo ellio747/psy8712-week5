85c8c3b165fdb28048d05753f53fee062c855d42
Ben Elliott <ellio747@umn.edu>
2026-02-17 16:11:05 -0600

    need L23 fix

--- R/week5.R
+++ R/week5.R
@@ -7,3 +7,18 @@ Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c(
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q",1:10)))
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = TRUE, col_types = "ic")
+
+# Data Cleaning
+Aclean_tbl <- Adata_tbl %>% 
+  separate(qs, into = paste0("q", 1:5), sep = "-") %>% 
+  mutate(datadate = mdy_hms(datadate)) %>% 
+  mutate(across(q1:q5, as.integer)) %>%
+  left_join(Anotes_tbl, by = "parnum") %>% 
+  filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>% 
+  mutate(datadate = mdy_hms(datadate)) %>% 
+  mutate(across(q1:q10, as.integer)) %>%
+  left_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>% 
+  bind_rows(mutate(Aclean_tbl, lab = "A")) %>% ## I have to figure this line out in order to add B to the Bdata lab column. 
+  select(-notes)
