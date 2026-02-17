09659266e081d620fc5720eec2d39592f1211f7b
Ben Elliott <ellio747@umn.edu>
2026-02-16 21:59:04 -0600

    Initial commit

---
+++ R/week5.R
@@ -0,0 +1,13 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import
+Adata_tbl <- 
+Anotes_tbl <- 
+Bdata_tbl <- 
+Bnotes_tbl <- 
+
+# Data Cleaning
+  
+# Col names col_names = c("casenum", "parnum", "stimver", "datadate", "qs")
